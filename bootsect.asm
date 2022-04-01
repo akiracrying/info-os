b0368ca272204dc29f2b156c4d3152f794ae0b8b
Timofey Tsvetkov <78951084+weinenwollen@users.noreply.github.com>
2022-04-01 19:00:50 +0300

    Update bootsect.asm

--- bootsect.asm
+++ bootsect.asm
@@ -1,18 +1,3 @@
-# To boot, first write this:
-#
-# yasm -p gas -f bin -o bootsect.tmp bootsect.asm
-# dd bs=31744 skip=1 if=bootsect.tmp of=bootsect.bin
-# g++ -ffreestanding -m32 -o kernel.o -c kernel.cpp
-# ////// ld --ignore-unresolved-symbol _GLOBAL_OFFSET_TABLE_ --oformat binary -Ttext 0x10000 -o kernel.bin --entry=kmain -m elf_i386 kernel.o 
-# ////// This is trash for ubuntu (ubuntu == govno mocha ebal ee rot)
-# ld --oformat binary -Ttext 0x10000 -o kernel.bin --entry=kmain -m elf_i386 kernel.o 
-#
-# Then, to boot:
-#
-# qemu -fda bootsect.bin -fdb kernel.bin
-#
-# Fine! You got it! :)
-
 .code16
 .org 0x7c00
 
